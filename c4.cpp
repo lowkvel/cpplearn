281a8a5d14fcbbe23674b51b0e0ff8fe7d974504
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-06-28 16:53:57 +0800

    Update c4.cpp

--- c4.cpp
+++ c4.cpp
@@ -4,14 +4,22 @@ using namespace std;
 
 int main()
 {
+    int tlist[3];
+    tlist[0] = 0;
+    tlist[1] = 1;
+    tlist[2] = 2;
+
+    int tlist2[3] = {0, 1, 2};
+
+    cout << sizeof(tlist) << endl;
+    cout << sizeof(tlist[0]) << endl;
+    cout << tlist[0] << " " << tlist[1] << " " << tlist[2] << endl;
 
 
 
 
 
-
-
-
+    return 0;
 
 }
 
