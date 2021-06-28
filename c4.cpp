013f97ea4bca54b6badd95f54408363dec1d4899
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-06-28 18:30:36 +0800

    Update c4.cpp

--- c4.cpp
+++ c4.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
@@ -8,15 +9,14 @@ int main()
     tlist[0] = 0;
     tlist[1] = 1;
     tlist[2] = 2;
-
     int tlist2[3] = {0, 1, 2};
-
     cout << sizeof(tlist) << endl;
     cout << sizeof(tlist[0]) << endl;
     cout << tlist[0] << " " << tlist[1] << " " << tlist[2] << endl;
 
-
-
+    char clist[] = "Hey";
+    cout << clist << " " << clist[0] << endl;
+    cout << strlen(clist) << " " << sizeof(clist) << endl;
 
 
     return 0;
