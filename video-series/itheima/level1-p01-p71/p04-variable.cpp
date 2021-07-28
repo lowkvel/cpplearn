b21d5155f3aef9e82287e9093381d4c8392d381c
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-28 11:09:39 +0800

    Create p04-variable.cpp

---
+++ video-series/itheima/level1-p01-p71/p04-variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// p04, variable
+int main() {
+
+    cout << "variable" << endl;
+
+    // variable declaration: variable_type variable_name = initial_value;
+    int a = 10;
+    cout << "int a = " << a << endl;
+    
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
