101a34f3b969d671274eecda3ceeaae7e3046f19
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-28 11:09:36 +0800

    Create p03-comment.cpp

---
+++ video-series/itheima/level1-p01-p71/p03-comment.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+// p03, comment
+int main() {
+
+    // one line comment
+    /* multiple line comments */
+    cout << "comment" << endl;
+    
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
