1ff1b13cb7c5875485e67d5ddfd3891a435923f8
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-28 11:10:28 +0800

    Create p06-keyword.cpp

---
+++ video-series/itheima/level1-p01-p71/p06-keyword.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// p06, keyword
+int main() {
+
+    cout << "keyword" << endl;
+
+    // cpp keyword list
+    // https://en.cppreference.com/w/cpp/keyword
+    
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
