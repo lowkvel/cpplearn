384ac7c28e7a4eb185308909b6ecbe537cafc2f5
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-28 11:09:33 +0800

    Create p02-cout.cpp

---
+++ video-series/itheima/level1-p01-p71/p02-cout.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+// p02, cout & endl, pause in windows
+int main() {
+    
+    cout << "hello world" << endl;
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
