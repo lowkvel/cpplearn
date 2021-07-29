8f2d83d468a88aa4480fc6b75513f36458a483f7
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-29 10:34:40 +0800

    Create p08-p15-datatype.cpp

---
+++ video-series/itheima/level1-p01-p71/p08-p15-datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+// p08-p15, datatype
+int main() {
+
+    cout << "datatype" << endl;
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
