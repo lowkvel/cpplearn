99cbb541a97f09f7fe8141ca83cb4d7a2563e2c4
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-29 09:48:20 +0800

    Create p07-identifier.cpp

---
+++ video-series/itheima/level1-p01-p71/p07-identifier.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// p07, identifier
+int main() {
+
+    cout << "identifier" << endl;
+
+    // cannot use keyword as identifier
+    //int int = 0;
+
+    // can only use alphabet character, digit or underscore in identifier
+    //int %d = 0;
+
+    // the first character must be an alphabet character or underscore in identifier
+    //int 90z = 0;
+    
+    // upper case and lower case are different in indentifier
+    //int Aa = 0;
+    
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
