e5a3e1af1b124e6260646e661eab99cb0449cfd3
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-30 11:39:04 +0800

    Create p24-p41-flow-control.cpp

---
+++ video-series/itheima/level1-p01-p71/p24-p41-flow-control.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+// p24-p41, flow control
+int main() {
+
+    cout << "flow control" << endl;
+
+    /*
+        Selection
+            if
+            switch
+
+        Iteration
+            while
+            do - while
+            for
+            range for
+
+        Jump
+            return
+            goto
+            continue
+            break
+    */
+    
+    
+
+
+
+
+    
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
