dde51f73cf91e14133c88fb42f07ef4a5ec9f885
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-30 11:20:04 +0800

    Create p16-p23-operator.cpp

---
+++ video-series/itheima/level1-p01-p71/p16-p23-operator.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+// p16-p23, operator
+int main() {
+
+    cout << "operator" << endl;
+
+    /*
+        https://en.cppreference.com/w/cpp/language/operators 
+
+        Arithmetic      + - * / % ++ --
+        Assignment      = += -= *= /= %= <<= >>= &= ^= |=
+        Relational      < <= == => > !=
+        Logical         && || !
+        Bitwise         & | ^ ~ << >> 
+        Misc            sizeof() ? 
+    */
+    
+
+
+
+
+
+
+    
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
