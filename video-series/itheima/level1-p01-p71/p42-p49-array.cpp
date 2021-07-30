49f28a8ba6dfc9f94722a6359d0ea59fe0e08823
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-30 16:24:01 +0800

    Create p42-p49-array.cpp

---
+++ video-series/itheima/level1-p01-p71/p42-p49-array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// p42-p49, array
+int main() {
+
+    cout << "array" << endl;
+
+    /*
+        array
+    */
+    
+
+    
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
