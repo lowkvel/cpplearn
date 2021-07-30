e28c4ef101c5c5374cc50a19c994aa127ae206f2
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-07-30 11:20:10 +0800

    arithmetic

--- video-series/itheima/level1-p01-p71/p16-p23-operator.cpp
+++ video-series/itheima/level1-p01-p71/p16-p23-operator.cpp
@@ -18,6 +18,18 @@ int main() {
         Misc            sizeof() ? 
     */
     
+    // arithmetic
+    // + - * pass
+    // / %
+    int a = 3; int b = 2; 
+    int c = a / b;                          // c = 1            int / int = int, b cannot be 0, discard the remainder
+    int d = a % b;                          // d = 1            int % int = int, b cannot be 0, can only do % on int
+    // ++
+    int a1 = 2; int b1 = ++a1;              // a = 3, b = 3     first a = a + 1,    then b = a
+    int a2 = 2; int b2 = a2++;              // a = 3, b = 2     first b = a,        then a = a + 1
+    // --
+    int a3 = 2; int b3 = --a3;              // a = 1, b = 1     first a = a - 1,    then b = a
+    int a4 = 2; int b4 = a4--;              // a = 1, b = 2     first b = a,        then a = a - 1
 
 
 
