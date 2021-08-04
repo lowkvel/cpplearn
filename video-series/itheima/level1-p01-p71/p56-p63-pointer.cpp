55101f8430adcd85d9bcc0f5c9a887c31631c3b5
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-04 12:49:46 +0800

    Create p56-p63-pointer.cpp

---
+++ video-series/itheima/level1-p01-p71/p56-p63-pointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// p56-p63, pointer
+int main() {
+
+    cout << "pointer" << endl;
+
+    /*
+        pointer
+
+
+    */
+
+
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
