176be5fa823f44ca9dd2a3c0977efb3e857a7702
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-04 20:27:11 +0800

    Create p64-p71-struct.cpp

---
+++ video-series/itheima/level1-p01-p71/p64-p71-struct.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// p64-p71, struct
+int main() {
+
+    cout << "struct" << endl;
+
+    /*
+        struct
+
+    */
+
+
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
