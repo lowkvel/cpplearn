65e401c587f0f192e877cb3cd5ae09f8b71dcca9
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-10 15:26:04 +0800

    Create p84-p87-memory.cpp

---
+++ video-series/itheima/level3-p84-p146/p84-p87-memory.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <limits>
+
+using namespace std;
+
+// p84-p87, memory
+int main() {
+
+    cout << "memory" << endl;
+
+    /*
+        memory
+
+
+    */
+    
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+
+}
