32b452e020e52507cba7856365f5de4095f4462b
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-18 14:44:53 +0800

    default argument

--- video-series/itheima/level3-p84-p146/p95-p98-function-adv.cpp
+++ video-series/itheima/level3-p84-p146/p95-p98-function-adv.cpp
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+void func1(int a, int b, int c);
+//void func2(int a, int b, int c);
+//void func3(int a, int b = 10, int c = 10);
+void func4(int a, int b = 10, int c = 10);
+
 // p95-p98, function adv
 int main() {
 
@@ -10,14 +15,46 @@ int main() {
     /*
         function adv
 
-
+        default argument of parameter
+            1.  void func(int a, int b, int c);
+                void func(int a, int b = 10, int c = 10){   // all parameters [b, c] after the first parameter [b] with default argument requires default arguments 
+                    ...
+                }
+            2.  void func(int a, int b = 10, int c = 10);
+                void func(int a, int b, int c){             // do not redefine default argument defined in declaration
+                    ...
+                }
 
     */
 
-
+    // default argument of parameter
+    //func1(10);    // ?
+    func1(10, 20, 30);  // default argument overrode
+    func4(10, 20, 30);
 
     //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
 
     return 0;
 }
 
+void func1(int a, int b = 10, int c = 10) {
+    cout << (a + b + c) << endl;
+}
+
+/*
+// starting from b, all parameters need default argument
+void func2(int a, int b = 10, int c) {
+    cout << (a + b + c) << endl;
+}
+*/
+
+/*
+// cannot redefine dufault arguments defined in declaration
+void func3(int a, int b = 10, int c = 10) {
+    cout << (a + b + c) << endl;
+}
+*/
+
+void func4(int a, int b, int c) {
+    cout << (a + b + c) << endl;
+}
