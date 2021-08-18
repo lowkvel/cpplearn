94f8cf4611fd71e22f26321a8eb7d5dbd829e38a
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-18 14:18:54 +0800

    Create p95-p98-function-adv.cpp

---
+++ video-series/itheima/level3-p84-p146/p95-p98-function-adv.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// p95-p98, function adv
+int main() {
+
+    cout << "function adv" << endl;
+
+    /*
+        function adv
+
+
+
+    */
+
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
