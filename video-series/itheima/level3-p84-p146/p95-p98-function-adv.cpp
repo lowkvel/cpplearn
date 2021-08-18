881a1609f9c48f69a3425bf73d6849dec9a0a1fd
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-18 14:56:44 +0800

    placeholder parameter

--- video-series/itheima/level3-p84-p146/p95-p98-function-adv.cpp
+++ video-series/itheima/level3-p84-p146/p95-p98-function-adv.cpp
@@ -7,6 +7,9 @@ void func1(int a, int b, int c);
 //void func3(int a, int b = 10, int c = 10);
 void func4(int a, int b = 10, int c = 10);
 
+void func5(int a, int);
+void func6(int a, int);
+
 // p95-p98, function adv
 int main() {
 
@@ -25,12 +28,28 @@ int main() {
                     ...
                 }
 
+        placeholder parameter
+            1.  void func1(int a, int);
+                void func1(int a, int) {
+                    ...
+                }
+                func1(10, 10);
+            2.  void func1(int a, int);
+                void func2(int a, int = 10) {               // weird
+                    ...
+                }
+                func2(10, 10);
+
     */
 
     // default argument of parameter
-    //func1(10);    // ?
+    //func1(10);          // ?
     func1(10, 20, 30);  // default argument overrode
     func4(10, 20, 30);
+
+    // placeholder parameter
+    func5(10, 10);      // have to fill a value for placeholder parameter
+    func6(10, 10);
 
     //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
 
@@ -57,4 +76,12 @@ void func3(int a, int b = 10, int c = 10) {
 
 void func4(int a, int b, int c) {
     cout << (a + b + c) << endl;
+}
+
+void func5(int a, int) {
+    cout << "why do we need placeholder parameter? idk." << endl;
+}
+
+void func6(int a, int = 10) {
+    cout << "placeholder parameter with default argument? weird." << endl;
 }
