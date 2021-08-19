5e07b9233e4c6d0a6651b18deabcd4f0c9f9803e
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-19 10:09:48 +0800

    Create p99-p105-oop-encapsulation.cpp

---
+++ video-series/itheima/level3-p84-p146/p99-p105-oop-encapsulation.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+// p99-p105, object encapsulation
+int main() {
+
+    cout << "object encapsulation" << endl;
+
+    /*
+        object encapsulation
+
+
+    */
+
+
+
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
