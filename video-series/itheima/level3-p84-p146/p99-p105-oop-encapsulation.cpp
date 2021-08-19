ec8ee2478f7f1f0ac7348316b2768a1437a3d408
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-19 11:15:21 +0800

    student class and set methods

--- video-series/itheima/level3-p84-p146/p99-p105-oop-encapsulation.cpp
+++ video-series/itheima/level3-p84-p146/p99-p105-oop-encapsulation.cpp
@@ -17,6 +17,28 @@ class Circle {
     }
 };
 
+class Student {
+    // access control
+    public:
+
+    // property
+    int id;
+    string name;
+
+    // behavior
+    void showStudent() {
+        cout << id << " " << name << endl;
+    }
+
+    void setId(int n) {
+        id = n;
+    }
+
+    void setName(string s) {
+        name = s;
+    }
+};
+
 // p99-p105, object encapsulation
 int main() {
 
@@ -25,6 +47,22 @@ int main() {
     /*
         object encapsulation
 
+        class class_name() {
+            // access control
+            public:             // access allowed within class, access allowed outside class
+            protected:          // access allowed within class, access disallowed outside class
+            private:            // access allowed within class, access disallowed outside class
+
+            // property
+            variable_type variable_name;
+            ...
+
+            // bahavior
+            return_type func() {
+                ...
+            }
+            ...
+        }
 
     */
 
@@ -33,6 +71,11 @@ int main() {
     c1.radius = 2;
     cout << c1.calculate_circumference() << endl;
 
+    // instantiate a Studeng object s1 and show its id and name
+    Student s1;
+    s1.setId(1);
+    s1.setName("Alice");
+    s1.showStudent();
 
 
     //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
