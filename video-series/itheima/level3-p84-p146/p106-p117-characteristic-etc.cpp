b23ef17206761dd22fbeb24c40c128a259b611b1
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-23 14:55:03 +0800

    Create p106-p117-characteristic-etc.cpp

---
+++ video-series/itheima/level3-p84-p146/p106-p117-characteristic-etc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// p106-p117, characteristics
+int main() {
+
+    cout << "characteristics" << endl;
+
+    /*
+        characteristics
+
+
+    */
+
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
