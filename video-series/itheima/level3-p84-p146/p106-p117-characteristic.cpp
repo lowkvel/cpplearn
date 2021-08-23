79eeee958c56384dcd1c793b528b2ceaf0796f3d
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-23 13:46:37 +0800

    3 types of constructor and 3 calling ways

--- video-series/itheima/level3-p84-p146/p106-p117-characteristic.cpp
+++ video-series/itheima/level3-p84-p146/p106-p117-characteristic.cpp
@@ -6,15 +6,30 @@ void func1();
 
 class Person {
 public:
-    // constructor
+    // default constructor
     Person() {
-        cout << "constructed" << endl;
+        age = 0;
+        cout << "default constructor, Person constructed" << endl;
+    }
+
+    // parameterized constructor
+    Person(int a) {
+        age = a;
+        cout << "parameterized constructor, Person constructed" << endl;
+    }
+
+    // copy constructor
+    Person(const Person &p) {
+        age = p.age;
+        cout << "copy constructor, Person constructed" << endl;
     }
 
     // destructor
     ~Person() {
-        cout << "destructed" << endl;
+        cout << "Person destructed" << endl;
     }
+
+    int age;
 
 };
 
@@ -30,6 +45,12 @@ int main() {
                         has same class_name
                         overloadable
                         automatically called by program once while instantiating a object (when?), no need for manual calling
+                        classfication:  default constructor (without parameter)
+                                        parameterized constructor (with parameter)
+                                        copy constructor (with another same class object as parameter)
+                        call:           ():      Person p1(...); or Person p1;
+                                        =:       Person p1 = Person(...); Person(...) is a anonymous object
+                                        {}       Person p1 = {...};
             class_name() {
                 ...
             }
@@ -52,6 +73,18 @@ int main() {
 }
 
 void func1() {
-    Person p1;
+    // ()
+    Person p1;      // default constructor, dont use () in it, otherwize compiler will treat it as function declaration
+    Person p2(10);  // parameterized constructor
+    Person p3(p2);  // copy constructor
+
+    // = 
+    Person p4 = Person();
+    Person p5 = Person(10);     //Person(...) is a anonymous object
+    Person p6 = Person(p5);
+    
+    // {}
+    Person p7 = 10;             // equivalent to Person p7 = Person(10); variant: Person p7 = {10}; <-- array
+    Person p8 = p7;             // equivalent to Person p8 = Person(p7);
 }
 
