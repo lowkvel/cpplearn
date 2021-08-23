b8739b24e63ef8a2d63ff6698821bf135cce7ea4
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-23 09:59:48 +0800

    constructor & destructor basic

--- video-series/itheima/level3-p84-p146/p106-p117-characteristic.cpp
+++ video-series/itheima/level3-p84-p146/p106-p117-characteristic.cpp
@@ -2,6 +2,22 @@
 
 using namespace std;
 
+void func1();
+
+class Person {
+public:
+    // constructor
+    Person() {
+        cout << "constructed" << endl;
+    }
+
+    // destructor
+    ~Person() {
+        cout << "destructed" << endl;
+    }
+
+};
+
 // p106-p117, characteristics
 int main() {
 
@@ -9,11 +25,33 @@ int main() {
 
     /*
         characteristics
+
+        constructor:    no return type nor void
+                        has same class_name
+                        overloadable
+                        automatically called by program once while instantiating a object (when?), no need for manual calling
+            class_name() {
+                ...
+            }
+        
+        destructor:     no return type nor void
+                        has same class_name but with '~' in front of it
+                        non-overloadable because parameters is not allowed
+                        automatically called by program once while destructing a object (when?), no need for manual calling
+            ~class_name() {
+                ...
+            }
     */
+
+    func1();
 
 
     //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
 
     return 0;
+}
+
+void func1() {
+    Person p1;
 }
 
