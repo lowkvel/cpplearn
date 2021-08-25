c67c097acb108ebd949db29e7187ea2fd6ebd00d
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-25 10:49:45 +0800

    Create p118-p120-friend.cpp

---
+++ video-series/itheima/level3-p84-p146/p118-p120-friend.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// p118-p120, friend
+int main() {
+
+    cout << "friend" << endl;
+
+    /*
+        friend
+
+
+
+        
+    */
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
