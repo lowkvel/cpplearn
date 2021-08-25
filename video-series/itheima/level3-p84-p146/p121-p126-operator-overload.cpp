076552bfa8848a7aed13a31c09e0fd9596e15264
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-25 16:25:05 +0800

    Create p121-p126-operator-overload.cpp

---
+++ video-series/itheima/level3-p84-p146/p121-p126-operator-overload.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+// p120-p126, operator overload
+int main() {
+
+    cout << "operator overload" << endl;
+
+    /*
+        operator overload
+
+        
+    */
+
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
