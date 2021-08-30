4a10d26feba87f2ce763f17627a4a68696631edb
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-08-30 14:59:35 +0800

    Create p127-p134-inheritance.cpp

---
+++ video-series/itheima/level3-p84-p146/p127-p134-inheritance.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+// p127-p134, inheritance
+int main() {
+
+    cout << "inheritance" << endl;
+
+    /*
+        inheritance
+
+
+    */
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
