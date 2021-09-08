b5fac6ed3ca32346195eeff424ea4b8752cee378
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-08 19:49:59 +0800

    Create p135-p142-polymorphism.cpp

---
+++ video-series/itheima/level3-p84-p146/p135-p142-polymorphism.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+// p135-p142, polymorphism
+int main() {
+
+    cout << "polymorphism" << endl;
+
+    /*
+        polymorphism
+
+
+
+    */
+
+
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
