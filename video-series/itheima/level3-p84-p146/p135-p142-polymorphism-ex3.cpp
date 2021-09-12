6efab6064ae64c5ed22892e415ba9d66a7a7764f
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-12 09:35:38 +0800

    Create p135-p142-polymorphism-ex3.cpp

---
+++ video-series/itheima/level3-p84-p146/p135-p142-polymorphism-ex3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+// p135-p142, polymorphism ex3
+int main() {
+
+    cout << "polymorphism ex3" << endl;
+
+    /*
+        polymorphism ex3
+    */
+
+    
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
