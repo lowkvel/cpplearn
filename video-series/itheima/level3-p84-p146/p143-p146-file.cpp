d9bcf715da8bee3bf83c7efaa676916c5fda5d19
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-13 09:21:50 +0800

    Create p143-p146-file.cpp

---
+++ video-series/itheima/level3-p84-p146/p143-p146-file.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// p143-p146, file
+int main() {
+
+    cout << "file" << endl;
+
+    /*
+        file
+    */
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
