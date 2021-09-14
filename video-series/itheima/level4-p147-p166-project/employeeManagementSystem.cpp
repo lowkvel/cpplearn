75ae08246313e3124e8398a0d08c51af3c6c1328
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-14 09:36:55 +0800

    Create employeeManagementSystem.cpp

---
+++ video-series/itheima/level4-p147-p166-project/employeeManagementSystem.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
