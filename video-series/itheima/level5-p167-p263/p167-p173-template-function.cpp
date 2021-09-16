a2cd6c22c6b2a2a7cfad395e91023849d6411c36
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-16 14:03:09 +0800

    Create p167-p173-template-function.cpp

---
+++ video-series/itheima/level5-p167-p263/p167-p173-template-function.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using namespace std;
+
+// p167-p173, template function
+int main() {
+
+    cout << "template function" << endl;
+
+    /*
+        template function
+
+    */
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
