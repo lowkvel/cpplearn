07304b1f23cc64debc4f37a6572b753cb9b459d7
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-18 09:31:22 +0800

    Create p167-p173-template-class.cpp

---
+++ video-series/itheima/level5-p167-p263/p167-p173-template-class.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+
+using namespace std;
+
+// p167-p173, template class
+int main() {
+
+    cout << "template class" << endl;
+
+    /*
+        template class
+
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
