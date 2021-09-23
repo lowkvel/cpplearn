3e6ee12d6a6870e73ae3ebdab89ab8970d9060a5
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-23 09:52:19 +0800

    Create p182-p184-template-ex.cpp

---
+++ video-series/itheima/level5-p167-p263/p182-p184-template-ex.cpp
@@ -0,0 +1,18 @@
+# include <iostream>
+
+using namespace std;
+
+// p167-p173, template ex
+int main() {
+
+    cout << "template ex" << endl;
+
+    /*
+        template ex
+    */
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
