65856a31bd4c9379e7f8e1d3f160359a89912348
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-23 14:28:42 +0800

    Create p185-p188-stl-basic.cpp

---
+++ video-series/itheima/level5-p167-p263/p185-p188-stl-basic.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p185-p188, stl basic
+int main() {
+
+    cout << "stl basic" << endl;
+
+    /*
+        stl basic
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
