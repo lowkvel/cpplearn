fce51dd3cb823217d0298a0596c12828badebd6b
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-24 09:16:51 +0800

    Create p189-p196-stl-string.cpp

---
+++ video-series/itheima/level5-p167-p263/p189-p196-stl-string.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+
+void func1();
+
+// p189-p196, stl string
+int main() {
+
+    cout << "stl string" << endl;
+
+    /*
+        stl string
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+    
+}
