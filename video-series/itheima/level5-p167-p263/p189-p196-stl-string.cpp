e9ab7c4918a7bfe90edb4ff22c151917ee735c4a
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-24 09:32:25 +0800

    constructor

--- video-series/itheima/level5-p167-p263/p189-p196-stl-string.cpp
+++ video-series/itheima/level5-p167-p263/p189-p196-stl-string.cpp
@@ -12,6 +12,27 @@ int main() {
 
     /*
         stl string
+
+        1.  constructor
+            1.  string();                   // create a empty string
+            2.  string(const char* s);      // create a string given const char *str
+            3.  string(const string &str);  // create a string given another string
+            4.  string(int n, char c);      // create a string given multiple same characters
+
+        2.  assignment
+
+        3.  concat
+
+        4.  find & replace
+
+        5.  compare
+
+        6.  set & get
+
+        7.  insert & delete
+
+        8.  substring
+
     */
 
     func1();
@@ -22,5 +43,16 @@ int main() {
 }
 
 void func1() {
-    
+    // string constructor
+    string s1;                  // 1.  string();                   // create a empty string
+    cout << s1 << endl;
+    const char *str = "str";
+    string s2(str);             // 2.  string(const char* s);      // create a string given const char *str
+    cout << s2 << endl;
+    string s3(s2);              // 3.  string(const string &str);  // create a string given another string
+    cout << s3 << endl;
+    string s4(10, 'a');         // 4.  string(int n, char c);      // create a string given multiple same characters
+    cout << s4 << endl;
+    string s5("str");           // ? prob 2 ?
+    cout << s5 << endl;
 }
