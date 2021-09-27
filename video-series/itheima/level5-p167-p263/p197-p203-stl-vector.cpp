2cc6bd830a22eb170fced4b8a6c3fdaea1e57185
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-27 09:51:19 +0800

    Create p197-p203-stl-vector.cpp

---
+++ video-series/itheima/level5-p167-p263/p197-p203-stl-vector.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p197-p203, stl vector
+int main() {
+
+    cout << "stl vector" << endl;
+
+    /*
+        stl vector
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+    
+}
