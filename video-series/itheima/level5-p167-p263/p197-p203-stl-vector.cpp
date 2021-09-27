0e72bd5bd714ac2eeed49da3005d07aea861d829
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-27 15:15:22 +0800

    constructor

--- video-series/itheima/level5-p167-p263/p197-p203-stl-vector.cpp
+++ video-series/itheima/level5-p167-p263/p197-p203-stl-vector.cpp
@@ -1,8 +1,10 @@
 # include <iostream>
+# include <vector>
 
 using namespace std;
 
 void func1();
+void vectorPrinter(vector <int> &v);
 
 // p197-p203, stl vector
 int main() {
@@ -11,6 +13,13 @@ int main() {
 
     /*
         stl vector
+        
+        1.  constructor
+            1.  vector<T> v;                   // default constructor
+            2.  vector(v.begin(), v.end());     // copy [v.begin(), v.end()), closed-open range
+            3.  vector(n, element);             // construct n * element
+            4.  vector(const vector &vec);      // copy constructor
+
     */
 
     func1();
@@ -21,5 +30,17 @@ int main() {
 }
 
 void func1() {
-    
+    // constructor
+    vector<int> v1;
+    for (int i = 0; i < 10; i++) v1.push_back(i);   vectorPrinter(v1);  // default constructor
+    vector<int> v2(v1.begin(), v1.end());           vectorPrinter(v2);  // copy [v.begin(), v.end()), closed-open range
+    vector<int> v3(10, 1);                          vectorPrinter(v3);  // construct n * element
+    vector<int> v4(v3);                             vectorPrinter(v4);  // copy constructor
+
+}
+
+void vectorPrinter(vector <int> &v) {
+    for (vector<int>::iterator it = v.begin(); it != v.end(); it++)
+        cout << *it << " ";
+    cout << endl;
 }
