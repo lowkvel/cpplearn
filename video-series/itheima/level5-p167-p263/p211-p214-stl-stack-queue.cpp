816fa354e49558b05e297ab73a38d508c36438b8
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-28 15:02:52 +0800

    Create p211-p214-stl-stack-queue.cpp

---
+++ video-series/itheima/level5-p167-p263/p211-p214-stl-stack-queue.cpp
@@ -0,0 +1,27 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p104-p209, stl stack & queue
+int main() {
+
+    cout << "stl stack & queue" << endl;
+
+    /*
+        stl stack & queue
+
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
+
