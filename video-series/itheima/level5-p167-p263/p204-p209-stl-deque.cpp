c489b0441eb8198803a06280cc165cf4f3e37284
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-28 14:12:47 +0800

    difference with vector, mechanism of deque, constructor

--- video-series/itheima/level5-p167-p263/p204-p209-stl-deque.cpp
+++ video-series/itheima/level5-p167-p263/p204-p209-stl-deque.cpp
@@ -1,8 +1,10 @@
 # include <iostream>
+# include <deque>
 
 using namespace std;
 
 void func1();
+void dequePrinter(const deque<int> &d);
 
 // p104-p209, stl deque
 int main() {
@@ -10,8 +12,20 @@ int main() {
     cout << "stl deque" << endl;
 
     /*
-        stl deque
-
+        stl deque (double-ended queue)
+        0.  difference with vector and its mechanism
+            1.  difference
+                1.  double-ended vector: insert/delete at both head and tail end 
+                2.  faster insert/detele time at head compared to vecter
+                3.  slower accessing time compared to vector
+            2.  mechanism
+                1.  deque has a bookkeeper, which stores a series of pointers points to multiple buffer area
+                2.  each buffer has limited capacity, and new buffer area will get allocated if current buffer area is filled
+        1.  constructor
+            1.  deque<T> d;                         // default constructor
+            2.  deque(d.begin(), d.end());          // copy [d.begin(), d.end()), closed-open range
+            3.  deque(n, element);                  // construct n * element
+            4.  deque(const deque &d);              // copy constructor
     */
 
     func1();
@@ -22,5 +36,16 @@ int main() {
 }
 
 void func1() {
+    // constructor
+    deque<int> d1;                                                      // default constructor
+    for (int i = 0; i < 10; i++) d1.push_back(i);   dequePrinter(d1);
+    deque<int> d2(d1.begin(), d1.end());            dequePrinter(d2);   // copy [d.begin(), d.end()), closed-open range
+    deque<int> d3(10, 7);                           dequePrinter(d3);   // construct n * element
+    deque<int> d4(d3);                              dequePrinter(d4);   // copy constructor
+}
 
+void dequePrinter(const deque<int> &d) {
+    for (deque<int>::const_iterator it = d.begin(); it != d.end(); it++)
+        cout << *it << " ";
+    cout << endl;
 }
