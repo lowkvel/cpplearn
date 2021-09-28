8933667390c9f2d67080224a95ddfd25e863fbc9
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-28 08:36:27 +0800

    Create p204-p209-stl-deque.cpp

---
+++ video-series/itheima/level5-p167-p263/p204-p209-stl-deque.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p104-p209, stl deque
+int main() {
+
+    cout << "stl deque" << endl;
+
+    /*
+        stl deque
+
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
