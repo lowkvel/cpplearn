7e8418026785108f412168f6296f61613c06bfdc
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-29 13:43:34 +0800

    Create p210-stl-ex1.cpp

---
+++ video-series/itheima/level5-p167-p263/p210-stl-ex1.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p210, stl ex1
+int main() {
+
+    cout << "stl ex1" << endl;
+
+    /*
+        stl ex1
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
+
