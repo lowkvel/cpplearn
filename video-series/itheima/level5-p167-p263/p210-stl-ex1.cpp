a353ccf920a9cbf83d8738481dfe111ba8187134
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-09-29 14:03:04 +0800

    Update p210-stl-ex1.cpp

--- video-series/itheima/level5-p167-p263/p210-stl-ex1.cpp
+++ video-series/itheima/level5-p167-p263/p210-stl-ex1.cpp
@@ -1,6 +1,21 @@
 # include <iostream>
+# include <vector>
+# include <deque>
+# include <algorithm>
 
 using namespace std;
+
+class Person {
+public:
+    string name;
+    int score;
+
+public:
+    Person(string name, int score) {
+        this->name = name;
+        this->score = score;
+    }
+};
 
 void func1();
 
@@ -21,6 +36,45 @@ int main() {
 }
 
 void func1() {
+    // set random seed
+    srand((unsigned int)time(NULL));
 
+    // initialize vector of Person
+    vector<Person> v;
+    string nameSeed = "abcde";
+    for (int i = 0; i < 5; i++) {
+        string name = "p";
+        name = name + nameSeed[i];
+
+        //Person p(name, 0);
+        v.push_back(Person(name, 0));
+    }
+
+    // printer
+    // for (vector<Person>::iterator it = v.begin(); it != v.end(); it++)
+    //     cout << (*it).name << " " << (*it).score << endl;
+
+    for (vector<Person>::iterator it = v.begin(); it != v.end(); it++) {     
+        // generate random 10 scores
+        deque<int> d;
+        for (int i = 0; i < 10; i++)
+            d.push_back(rand() % 41 + 60);  // (60, 100]
+        
+        // sort
+        sort(d.begin(), d.end());   
+
+        // delete min & max
+        d.pop_front();
+        d.pop_back();
+
+        // calculate avg & assignment
+        int sum = 0;
+        for (deque<int>::iterator dit = d.begin(); dit != d.end(); dit++) 
+            sum = sum + *dit;
+        (*it).score = sum / d.size();
+    }
+
+    //printer
+    for (vector<Person>::iterator it = v.begin(); it != v.end(); it++)
+        cout << (*it).name << " " << (*it).score << endl;
 }
-
