a46e0bf4567ed4a3920283e389509d717426ba26
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-11 16:11:48 +0800

    Create p231-p235-stl-map.cpp

---
+++ video-series/itheima/level5-p167-p263/p231-p235-stl-map.cpp
@@ -0,0 +1,26 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p231-p235, stl map
+int main() {
+
+    cout << "stl map" << endl;
+
+    /*
+        stl map
+
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
