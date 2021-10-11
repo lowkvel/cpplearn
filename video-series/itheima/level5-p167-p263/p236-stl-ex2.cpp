8d8a47ce84743e02811a1c7683987bd3dd8470fc
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-11 17:25:28 +0800

    Create p236-stl-ex2.cpp

---
+++ video-series/itheima/level5-p167-p263/p236-stl-ex2.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p236, stl ex2
+int main() {
+
+    cout << "stl ex2" << endl;
+
+    /*
+        stl ex2
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
