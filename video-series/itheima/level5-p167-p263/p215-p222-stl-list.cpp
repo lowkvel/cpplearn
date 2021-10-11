c76de769bdb4635803d86b043e13eab6f34407c4
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-11 11:21:31 +0800

    insert & delete

--- video-series/itheima/level5-p167-p263/p215-p222-stl-list.cpp
+++ video-series/itheima/level5-p167-p263/p215-p222-stl-list.cpp
@@ -30,6 +30,18 @@ int main() {
             2.  size();                         // current size
             3.  resize(int n);                  // resize size into n, delete the oversized element at the end or fill with default value
             4.  resize(int n, element);         // resize size into n, delete the oversized element at the end or fill with given value element
+        4.  insert & delete
+            1.  push_front(e);                                      // add element e at the head
+            2.  push_back(e);                                       // add element e at the end
+            3.  pop_front();                                        // delete the first element
+            4.  pop_back();                                         // delete the last element
+            5.  insert(const_iterator p, e)                         // insert element e at position p
+            6.  insert(const_iterator p, int n, e);                 // insert n * element e at position p
+            7.  insert(p, begin, end);                              // insert [d.begin(), d.end()) at position p
+            8.  erase(const_iterator p);                            // delete element where iterator points
+            9.  erase(const_iterator start, const_iterator end);    // delete elements from start to end
+            10. remove(e);                                          // delete all elements matching e
+            11. clear();                                            // delete all
     */
 
     func1();
@@ -60,6 +72,25 @@ void func1() {
     l8.resize(20, 1);   listPrinter(l8);                // expansion with given value 1
     l8.resize(5);       listPrinter(l8);                // shrinking
     l8.resize(10);      listPrinter(l8);                // re-expansion with default value 0
+
+    // insert & delete
+    list<int> l9 = l1;                              listPrinter(l9);
+    l9.push_front(7);                               listPrinter(l9);    // push_front
+    l9.push_back(7);                                listPrinter(l9);    // push_back
+    l9.pop_front();                                 listPrinter(l9);    // pop_front
+    l9.pop_back();                                  listPrinter(l9);    // pop_back
+    list<int>::iterator it1 = l9.begin(); it1++; 
+    l9.insert(it1, 7);                              listPrinter(l9);    // insert
+    list<int>::iterator it2 = l9.begin(); it2++;
+    l9.insert(it2, 5, 7);                           listPrinter(l9);    // insert
+    l9.erase(--it1);                                listPrinter(l9);    // erase
+    list<int>::iterator it31 = l9.begin(); it31++;
+    list<int>::iterator it32 = l9.end(); it32--;
+    l9.erase(it31, it32);                           listPrinter(l9);    // erase
+    l9.push_back(99); l9.push_front(99);            listPrinter(l9);
+    l9.remove(99);                                  listPrinter(l9);    // remove
+    l9.clear();                                     listPrinter(l9);    // clear
+    
 }
 
 void listPrinter(const list<int> &l) {
