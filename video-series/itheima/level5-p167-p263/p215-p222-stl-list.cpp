990b94139ec56e24644dc8d7cd55d0e3d01d2e66
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-11 10:48:21 +0800

    constructor

--- video-series/itheima/level5-p167-p263/p215-p222-stl-list.cpp
+++ video-series/itheima/level5-p167-p263/p215-p222-stl-list.cpp
@@ -1,8 +1,11 @@
 # include <iostream>
 
+# include <list>
+
 using namespace std;
 
 void func1();
+void listPrinter(const list<int> &l);
 
 // p215-p222, stl list
 int main() {
@@ -11,6 +14,12 @@ int main() {
 
     /*
         stl list
+
+        1.  constructor
+            1.  list<T> l;                  // default constructor
+            2.  list(l.begin(), l.end)();   // copy [begin, end), closed-open range
+            3.  list(n, element);           // construct n * element
+            4.  list(const list &l);        // copy constructor
     */
 
     func1();
@@ -21,5 +30,17 @@ int main() {
 }
 
 void func1() {
+    // constructor
+    list<int> l1;
+    for (int i = 0; i < 10; i++) l1.push_back(i);   listPrinter(l1);    // default
+    list<int> l2(l1.begin(), l1.end());             listPrinter(l2);    // closed-open range copy
+    list<int> l3(10, 1);                            listPrinter(l3);    // n element
+    list<int> l4(l3);                               listPrinter(l4);    // pure copy
 
+}
+
+void listPrinter(const list<int> &l) {
+    for (list<int>::const_iterator it = l.begin(); it != l.end(); it++)
+        cout << *it << " ";
+    cout << endl;
 }
