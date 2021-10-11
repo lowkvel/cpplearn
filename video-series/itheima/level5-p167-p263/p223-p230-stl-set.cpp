73aacb3f38407f459a61e9af259e8a1ead97ea7a
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-11 13:47:51 +0800

    Create p223-p230-stl-set.cpp

---
+++ video-series/itheima/level5-p167-p263/p223-p230-stl-set.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p223-p230, stl set
+int main() {
+
+    cout << "stl set" << endl;
+
+    /*
+        stl set
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
