91bc8ab194ea00d4854d8e76f36b412fc0e7e15f
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-11 14:27:42 +0800

    insert & delete

--- video-series/itheima/level5-p167-p263/p223-p230-stl-set.cpp
+++ video-series/itheima/level5-p167-p263/p223-p230-stl-set.cpp
@@ -23,9 +23,15 @@ int main() {
             2.  set(const set &s);              // copy constructor
             3.  set& operator=(const set &s);   // operator= overload
         2.  size & swap
-            1.  empty();        // is empty?
-            2.  size();         // current size
-            3.  swap(s);        // swap two sets
+            1.  empty();                        // is empty?
+            2.  size();                         // current size
+            3.  swap(s);                        // swap two sets
+        3.  insert & delete
+            1.  insert(element);                // insert a element
+            2.  erase(position);                // delete element at position p
+            3.  erase(s.begin(), s.end());      // delete closed-open range elements
+            4.  erase(element);                 // delete specific element
+            5.  clear();                        // delete all
     */
 
     func1();
@@ -48,6 +54,15 @@ void func1() {
     set<int> s5; for (int i = 5; i < 9; i++) s5.insert(i);  setPrinter(s5);
     s4.swap(s5); setPrinter(s4); setPrinter(s5);
     
+    // insert & delete
+    set<int> s6; for (int i = 0; i < 9; i++) s6.insert(i);  setPrinter(s6);
+    set<int>::iterator it1 = s6.begin(); it1++; it1++;
+    s6.erase(it1);  setPrinter(s6);                 // 2 (position 2) deleted
+    s6.erase(6);    setPrinter(s6);                 // 6 deleted
+    set<int>::iterator it21 = s6.begin(); it21++; it21++;
+    set<int>::iterator it22 = s6.end(); it22--; it22--;
+    s6.erase(it21, it22); setPrinter(s6);           // range position [2-5) deleted
+    s6.clear();     setPrinter(s6);                 // delete all
 }
 
 void setPrinter(const set<int> s) {
