c280d3d2ac85b37acad661310c4d8e712f233d79
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-11 13:57:43 +0800

    constructor & assignment

--- video-series/itheima/level5-p167-p263/p223-p230-stl-set.cpp
+++ video-series/itheima/level5-p167-p263/p223-p230-stl-set.cpp
@@ -1,8 +1,10 @@
 # include <iostream>
+# include <set>
 
 using namespace std;
 
 void func1();
+void setPrinter(const set<int> s);
 
 // p223-p230, stl set
 int main() {
@@ -11,6 +13,15 @@ int main() {
 
     /*
         stl set
+
+        0.  background
+            1.  auto sort for insert (binary tree implementation)
+            2.  set: not-allow for duplicates (duplicates omitted?)
+                multiset: allow for duplicates
+        1.  constructor & assignment
+            1.  set<T> s;                       // default constructor
+            2.  set(const set &s);              // copy constructor
+            3.  set& operator=(const set &s);   // operator= overload
     */
 
     func1();
@@ -21,5 +32,15 @@ int main() {
 }
 
 void func1() {
+    // constructor & assignment
+    set<int> s1;
+    for (int i = 0; i < 5; i++) s1.insert(i);   setPrinter(s1);     // using insert to add element to set, auto-sort, no-duplicates (duplicates ommited?)
+    set<int> s2(s1);                            setPrinter(s2);     // copy constructor
+    set<int> s3; s3 = s1;                       setPrinter(s3);     // operator= overload
+}
 
+void setPrinter(const set<int> s) {
+    for (set<int>::const_iterator it = s.begin(); it != s.end(); it++) 
+        cout << *it << " ";
+    cout << endl;
 }
