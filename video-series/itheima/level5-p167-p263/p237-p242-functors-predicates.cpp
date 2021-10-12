e7fc8342f09a0d96dda7c8f17d5246a4851cb009
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-12 10:18:29 +0800

    Create p237-p242-functors-predicates.cpp

---
+++ video-series/itheima/level5-p167-p263/p237-p242-functors-predicates.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p237-p242, functors & predicates
+int main() {
+
+    cout << "functors & predicates" << endl;
+
+    /*
+        functors & predicates
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
