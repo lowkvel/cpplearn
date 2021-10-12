6373ee8a899aac51620b493c097f3456c5dc38c5
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-12 14:18:32 +0800

    functor characteristics

--- video-series/itheima/level5-p167-p263/p237-p242-functors-predicates.cpp
+++ video-series/itheima/level5-p167-p263/p237-p242-functors-predicates.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+class myAdd {
+public:
+    int operator()(int v1, int v2) {
+        return v1 + v2;
+    }
+};
+
+class myPrint {
+public:
+    int count;      // counter
+
+public:
+    myPrint() {
+        this->count = 0;
+    }
+
+    void operator()(string s) {
+        cout << s << endl;
+        this->count++;
+    }
+};
+
 void func1();
+void doPrint(myPrint &mp, string s);
 
 // p237-p242, functors & predicates
 int main() {
@@ -11,8 +34,15 @@ int main() {
 
     /*
         functors & predicates
+
+        1.  characteristics
+            1.  use it like normal function, it can have parameter or return value
+            2.  functors can have its own (internal) states
+            3.  functors can get passed as parameters into other functions
+
     */
 
+    // 1.   characteristics
     func1();
 
     //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
@@ -21,5 +51,19 @@ int main() {
 }
 
 void func1() {
+    // use it like normal function, it can have parameter or return value
+    myAdd ma;
+    cout << ma(1, 2) << endl;
 
+    // functors can have its own (internal) states
+    myPrint mp;
+    for (int i = 0; i < 3; i++) mp("functors");
+    cout << "functors got printed " << mp.count << " times" << endl;
+
+    // functors can get passed as parameters into other functions
+    myPrint mp2;
+    doPrint(mp2, "functors2");
+    cout << "functors2 got printed " << mp2.count << " times" << endl;
 }
+
+void doPrint(myPrint &mp, string s) { mp(s); }
