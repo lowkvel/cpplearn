0b32856eef741ed2cdb8791e4fd0c4f1de64e0bd
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-12 10:06:40 +0800

    ex2

--- video-series/itheima/level5-p167-p263/p236-stl-ex2.cpp
+++ video-series/itheima/level5-p167-p263/p236-stl-ex2.cpp
@@ -1,8 +1,21 @@
 # include <iostream>
+# include <vector>
+# include <map>
+# include <ctime>
 
 using namespace std;
 
+class Worker {
+public:
+    string name;
+    int wage;
+};
+
 void func1();
+void printWorkers(const vector<Worker> &vw);
+void workerCreation(vector<Worker> &vw);
+void workerPlacement(const vector<Worker> &vw, multimap<int, Worker> &mw);
+void workerDisplayByPlacement(const multimap<int, Worker> &mw);
 
 // p236, stl ex2
 int main() {
@@ -21,5 +34,51 @@ int main() {
 }
 
 void func1() {
+    // srand
+    srand((unsigned int) time (NULL));
+    
+    // create 10 workerss
+    vector<Worker> vw;
+    workerCreation(vw);
+    
+    // worker placement
+    multimap<int, Worker> mw;
+    workerPlacement(vw, mw);
 
+    // worker display by placement
+    workerDisplayByPlacement(mw);
+}
+
+void printWorkers(const vector<Worker> &vw) {
+    for (vector<Worker>::const_iterator it = vw.begin(); it != vw.end(); it++)
+        cout << it->name << " " << it->wage << endl;
+}
+
+void workerCreation(vector<Worker> &vw) {
+    string nameSeed = "ABCDEFGHIJKLMN";
+    for (int i = 0; i < 10; i++) {
+        Worker w;
+        w.name = "wk";
+        w.name += nameSeed[i];
+        w.wage = rand() % 10000 + 10000;    // 10000-19999
+
+        vw.push_back(w);
+    }
+}
+
+void workerPlacement(const vector<Worker> &vw, multimap<int, Worker> &mw) {
+    for (vector<Worker>::const_iterator it = vw.begin(); it != vw.end(); it++) {
+        int deptid = rand() % 3;            // 0, 1, 2
+        mw.insert(make_pair(deptid, *it));
+    }
+}
+
+void workerDisplayByPlacement(const multimap<int, Worker> &mw) {
+    for (int i = 0; i < 3; i++) {
+        // get the iterator of first occurence of key i
+        // count how many got outputed, compare with how many needs to output
+        int outputed = 0;
+        for (multimap<int, Worker>::const_iterator pos = mw.find(i); pos != mw.end() && outputed < mw.count(i); pos++, outputed++) 
+            cout << (*pos).first << " " << (*pos).second.name << " " << (*pos).second.wage << endl;
+    }
 }
