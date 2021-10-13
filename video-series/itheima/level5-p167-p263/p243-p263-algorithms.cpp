0453545ccde1439bb25c049c3a79ab9152bd2fb3
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-13 11:05:19 +0800

    search

--- video-series/itheima/level5-p167-p263/p243-p263-algorithms.cpp
+++ video-series/itheima/level5-p167-p263/p243-p263-algorithms.cpp
@@ -6,10 +6,27 @@
 
 using namespace std;
 
+class Person {
+public:
+    string name;
+    int age;
+
+public:
+    Person(string name, int age) {
+        this->name = name;
+        this->age = age;
+    }
+
+    bool operator==(const Person &p) { return this->name == p.name && this->age == p.age; }
+};
+
 class printIntc { public: void operator()(int v) { cout << v << " "; } };
 class Mirror { public: int operator()(int v) { return -v; } };
+class greaterFive { public: bool operator()(int v) { return v > 5; } };
+class greaterFivep { public: bool operator()(Person &p) { return p.age > 5; } };
 
 void func1();   // traverse algorithms
+void func2();   // search algorithms
 void printInt(int v);
 
 // p243-p263, algorithms
@@ -27,9 +44,17 @@ int main() {
         1.  traverse algorithms
             1.  for_each
             2.  transform
+        2.  search algorithms
+            1.  find(iterator_begin, iterator_end, value);              // operator== overload required if item is not built-in datatype
+            2.  find_if(iterator_begin, iterator_end, _Predicate);
+            3.  adjacent_find(iterator_begin, iterator_end);
+            4.  binary_search(iterator_begin, iterator_end, value); 
+            5.  count(iterator_begin, iterator_end, value); 
+            6.  count_if(iterator_begin, iterator_end, _Predicate);
     */
 
     func1();    // traverse algorithms
+    func2();    // search algorithms
 
     //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
 
@@ -48,6 +73,45 @@ void func1() {
     transform(v2.begin(), v2.end(), v3.begin(), Mirror());          // functor Mirror()
     for_each(v2.begin(), v2.end(), printIntc());    cout << endl;   // functor printIntc()
     for_each(v3.begin(), v3.end(), printIntc());    cout << endl;   // functor printIntc()
+}
+
+void func2() {
+    vector<int> v1; for (int i = 0; i < 9; i++) v1.push_back(i); v1.push_back(8);
+    vector<Person> v2;
+    Person p1("a", 1); v2.push_back(p1);
+    Person p2("b", 2); v2.push_back(p2);
+    Person p3("c", 3); v2.push_back(p3); v2.push_back(p3);
+    Person p4("d", 4);
+
+    // search algorithm, find
+    vector<int>::iterator it11 = find(v1.begin(), v1.end(), 7);
+    it11 == v1.end() ? cout << "7 not found" << endl : cout << "7 found" << endl;
+    // operator== overload required because of user-defined Person data type
+    vector<Person>::iterator it22 = find(v2.begin(), v2.end(), p4);
+    it22 == v2.end() ? cout << "p4 not found" << endl : cout << "p4 found" << endl;
+
+    // search algorithm, find_if
+    vector<int>::iterator it31 = find_if(v1.begin(), v1.end(), greaterFive());
+    it31 == v1.end() ? cout << ">5 not found" << endl : cout << ">5 found" << endl;
+    vector<Person>::iterator it32 = find_if(v2.begin(), v2.end(), greaterFivep());
+    it31 == v1.end() ? cout << ">5p not found" << endl : cout << ">5p found" << endl;
+
+    // search algorithm, adjacent_find
+    vector<int>::iterator it41 = adjacent_find(v1.begin(), v1.end());
+    it31 == v1.end() ? cout << "adjacent duplicates not found" << endl : cout << "adjacent duplicates found" << endl;
+    //vector<int>::iterator it42 = adjacent_find(v2.begin(), v2.end());
+
+    // search algorithm, binary_search
+    cout << binary_search(v1.begin(), v1.end(), 7) << endl;
+    //cout << binary_search(v2.begin(), v2.end(), p2) << endl;
+
+    // search algorithm, count
+    cout << count(v1.begin(), v1.end(), 8) << endl;
+    cout << count(v2.begin(), v2.end(), p3) << endl;
+
+    // search algorithm, count_if
+    cout << count_if(v1.begin(), v1.end(), greaterFive()) << endl;
+    cout << count_if(v2.begin(), v2.end(), greaterFivep()) << endl;
 }
 
 void printInt(int v) { cout << v << " "; }
