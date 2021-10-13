020e3342b43d10e7ece815840bb231847cf8c72e
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-13 09:19:40 +0800

    Create p243-p263-algorithms.cpp

---
+++ video-series/itheima/level5-p167-p263/p243-p263-algorithms.cpp
@@ -0,0 +1,25 @@
+# include <iostream>
+
+using namespace std;
+
+void func1();
+
+// p243-p263, algorithms
+int main() {
+
+    cout << "algorithms" << endl;
+
+    /*
+        algorithms
+    */
+
+    func1();
+
+    //system("pause");    // use it in windows, effect [press any key to continue], no such command in linux/mac
+
+    return 0;
+}
+
+void func1() {
+
+}
