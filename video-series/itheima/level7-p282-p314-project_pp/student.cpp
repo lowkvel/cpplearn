9bfe1757142ff96f36260623bdcf697d4079f94a
cy2333 <35268618+lowkvel@users.noreply.github.com>
2021-10-22 15:31:26 +0800

    makeReservation

--- video-series/itheima/level7-p282-p314-project_pp/student.cpp
+++ video-series/itheima/level7-p282-p314-project_pp/student.cpp
@@ -10,6 +10,8 @@ Student::Student(int id, string name, string password) {
     this->id = id;
     this->name = name;
     this->password = password;
+
+    this->vcInit();
 }
 
 void Student::operationMenu() {
@@ -23,8 +25,54 @@ void Student::operationMenu() {
     cout << "-----------------------------------" << endl;
 }
 
-void Student::makeReservation() {
+// init vc vector
+void Student::vcInit() {
+    ifstream ifs;
+    ifs.open(COMPUTER_FILE, ios::in);
 
+    ComputerRoom cr;
+    while (ifs >> cr.id && ifs >> cr.maxNum) vc.push_back(cr);
+    ifs.close();
+}
+
+void Student::makeReservation() {
+    cout << "Please choose a date" << endl;
+    cout << "1. Mon; 2. Tue; 3. Wed; 4. Thu; 5. Fri: ";
+    int date = 0;
+    while (true) {
+        cin >> date;
+        if (date >= 1 && date <= 5) break;
+        cout << "\nWrong input." << endl;
+    }
+
+    cout << "Please choose a time" << endl;
+    cout << "1. Morning; 2. Afternoon: ";
+    int time = 0;
+    while (true) {
+        cin >> time;
+        if (time >= 1 && time <= 2) break;
+        cout << "\nWrong input." << endl;
+    }
+
+    int room = 0;
+    cout << "Please choose a time" << endl;
+    cout << "1. 20; 2. 50; 3. 100: ";
+    while (true) {
+        cin >> room;
+        if (room >= 1 && room <= 3) break;
+        cout << "\nWrong input." << endl;
+    }
+
+    cout << "Reservation made, waiting for validation" << endl;
+
+    ofstream ofs(RESERVATION_FILE, ios::app);
+    ofs << "date:" << date << " ";
+    ofs << "time:" << time << " ";
+    ofs << "stuid:" << this->id << " ";
+    ofs << "stuname:" << this->name << " ";
+    ofs << "roomid:" << room << " ";
+    ofs << "status:" << 1 << endl;      // status 1, under review
+    ofs.close();
 }
 
 void Student::showMyReservations() {
--- video-series/itheima/level7-p282-p314-project_pp/student.h
+++ video-series/itheima/level7-p282-p314-project_pp/student.h
@@ -1,16 +1,21 @@
 # pragma once
 
 # include <iostream>
+# include <fstream>
+# include <vector>
 
+# include "globalConstants.h"
+# include "computerRoom.h"
 # include "user.h"
 
 using namespace std;
 
 class Student: public User {
-/*
+
 public: 
     // member fields [int id, string name, string password] inherited from parent User
-*/
+    vector<ComputerRoom> vc;
+
 
 public:
     Student();
@@ -19,6 +24,8 @@ public:
     
     virtual void operationMenu();   // overrode pure virtual function from parent class User makes Student instantiable
     
+    void vcInit();
+
     void makeReservation();         // make a reservation
     
     void showMyReservations();      // show my reservations
