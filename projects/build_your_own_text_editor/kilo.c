e1614fed9a38426341262f11110783c5ca2e729a
cy2333 <lowkvel@gmail.com>
2021-12-13 15:58:58 +0800

    entering raw mode, flags flipping

--- projects/build_your_own_text_editor/kilo.c
+++ projects/build_your_own_text_editor/kilo.c
@@ -27,13 +27,15 @@ void enableRawMode() {
     // initiaze a temp struct termios variable raw to original_termios for later modification
     struct termios raw = original_termios;
 
-    // modifythe termios struct to ~(ECHO)
-    raw.c_lflag &= ~(ECHO | ICANON);
+    // modifythe termios struct
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK| ISTRIP| IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     /*
         The c_lflag field is for “local flags”. 
         A comment in macOS’s <termios.h> describes it as a “dumping ground for other state”. 
         So perhaps it should be thought of as “miscellaneous flags”. 
-        
         The other flag fields are 
             c_iflag (input flags), 
             c_oflag (output flags), 
@@ -49,6 +51,35 @@ void enableRawMode() {
         ICANON flag that allows us to turn off canonical mode. 
         Now we are in raw mode, 
         This means we will finally be reading input byte-by-byte, instead of line-by-line.
+
+        ISIG flag disable Ctrl-C/Z signals and Ctrl-Y on macOS
+        Ctrl-C sends a SIGINT signal to the current process which causes it to terminate, 
+        Ctrl-Z sends a SIGTSTP signal to the current process which causes it to suspend
+
+        IXON flag disable Ctrl-S/Q signals from old day software flow control
+        Ctrl-S stops data from being transmitted to the terminal until you press Ctrl-Q
+
+        IEXTEN flag disable Ctrl-V signal
+        Ctrl-V the terminal waits for you to type another character and then sends that character literally
+
+        ICRNL flag fix Ctrl-M signal
+
+        OPOST flag disable all output processing
+        the terminal does a similar translation on the output side. 
+        It translates each newline ("\n") we print into a carriage return followed by a newline ("\r\n"). 
+        The terminal requires both of these characters in order to start a new line of text. 
+        The carriage return moves the cursor back to the beginning of the current line, 
+        and the newline moves the cursor down a line, scrolling the screen if necessary. 
+
+        BRKINT, INPCK, ISTRIP, and CS8 all come from <termios.h>
+        1.  When BRKINT is turned on, a break condition will cause a SIGINT signal to be sent to the program, 
+            like pressing Ctrl-C.
+        2.  INPCK enables parity checking, which doesn’t seem to apply to modern terminal emulators.
+        3.  ISTRIP causes the 8th bit of each input byte to be stripped, 
+            meaning it will set it to 0. This is probably already turned off.
+        4.  CS8 is not a flag, it is a bit mask with multiple bits, 
+            which we set using the bitwise-OR (|) operator unlike all the flags we are turning off. 
+            It sets the character size (CS) to 8 bits per byte. 
     */                   
     
     // set a terminal’s attributes, TCSAFLUSH
@@ -66,10 +97,10 @@ int main() {
 
     char c;
     while (read(STDERR_FILENO, &c, 1) == 1 && c != 'q') {
-        if (iscntrl(c)) {       // iscntrl() tests for control character (nonprintable).
-            printf("%d\n", c);
+        if (iscntrl(c)) {                   // iscntrl() tests for control character (nonprintable)
+            printf("%d\r\n", c);            // added carriage return \r because of OPOST turned off
         } else {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);  // added carriage return \r because of OPOST turned off
         }
     }
     /* 
