1b9aa287b1769ae9fe395534d8681b62790d3089
cy2333 <lowkvel@gmail.com>
2021-12-13 14:10:22 +0800

    unistd.h, read() & return value, canonical mode

--- projects/build_your_own_text_editor/kilo.c
+++ projects/build_your_own_text_editor/kilo.c
@@ -1,3 +1,18 @@
+# include <unistd.h>
+
 int main() {
+    char c;
+
+    /* 
+        asking read() to read 1 byte from the standard input into the variable c, 
+        and to keep doing it until there are no more bytes to read
+
+        read() returns the number of bytes that it read (now should be 1), 
+        and will return 0 when it reaches the end of a file.
+
+        by default the terminal starts in canonical mode, also called cooked mode.
+    */
+    while (read(STDERR_FILENO, &c, 1) == 1);
+
     return 0;   // A return value of 0 indicates success
 }
